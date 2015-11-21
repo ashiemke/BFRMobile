d85a015c045287f56640b2596aafddfd8ad6d9e3
Adam Shiemke <ashiemke@allegion.com>
2015-11-21 09:56:07 -0700

    added SB

--- MobileRobot/LoginViewController.m
+++ MobileRobot/LoginViewController.m
@@ -6,13 +6,13 @@
 //  Copyright © 2015 Adam Shiemke. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LoginViewController.h"
 
-@interface ViewController ()
+@interface LoginViewController ()
 
 @end
 
-@implementation ViewController
+@implementation LoginViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
